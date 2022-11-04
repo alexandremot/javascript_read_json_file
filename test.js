436545b5812b278ae7862e105789926b011df5b3
Alexandre Mota <alexandremot@gmail.com>
2022-11-04 20:31:59 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
+'use strict';
 
+const fs = require('fs');
+
+fs.readFile('dados_chave.json', (err, data) => {
+    if (err) throw err;
+    let my_json = JSON.parse(data);
+    console.log(my_json[0].chave);
+});
